9d2a118ba3e2f2456048ee04d342ffd0b9bc1775
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-17 23:16:40 +0000

    added task 8 web framework

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Web framework for hbnb"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def state_list_by_cities():
+    state_objs = storage.all(State)
+    city_objs = storage.all(City)
+    state_list = sorted(state_objs.values(), key=lambda x: x.name)
+    lst = list()
+    final_states_list = list()
+    for state in state_list:
+        for city in city_objs:
+            if city.state_id == state.id:
+                lst.append(city)
+        flst = sorted(lst, key=lambda x: x.name)
+        final_states_list.append((state, list(flst)))
+        lst.clear()
+        flst.clear()
+
+    return render_template('7-states_list.html', states=final_states_list)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    if storage:
+        storage.close()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
