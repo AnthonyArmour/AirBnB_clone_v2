71d32fa7e50ca5a85d726aa6e22cd6bba6ac9f2a
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-17 16:22:45 +0000

    added task 1 web framework

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Web framework for hbnb"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
