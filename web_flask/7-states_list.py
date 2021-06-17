1651602dbbfcc51471dd21362f26f5a5099af5d9
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-17 21:34:15 +0000

    debug task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,11 +9,16 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def state_list():
     objs = storage.all(State)
     lst_objs = sorted(objs.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=lst_objs)
+
+
+@app.teardown_appcontext
+def teardown_appcontext():
+    storage.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
