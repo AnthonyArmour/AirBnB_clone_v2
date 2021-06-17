2d48dfd502a72c4f20035dc4421a99ad3fc81b1e
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-17 22:22:56 +0000

    another debug for task 7 web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,7 +19,7 @@ def state_list():
 @app.teardown_appcontext
 def tear_down(exception):
     if storage:
-        storage.__session.close()
+        storage.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
