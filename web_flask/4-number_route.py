431124a1ad15962a76c43b292b61011d1e97bccf
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-17 17:01:18 +0000

    added task 4 web framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Web framework for hbnb"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route('/python/<string:text>')
+@app.route('/python/')
+def python_text(text="is cool"):
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def n_num(n):
+    return "%d is a number" % n
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
